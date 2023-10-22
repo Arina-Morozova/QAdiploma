7032267a33573fb3ec12b8278d02ef6209ede156
Neto test <aryamorozkova555@gmail.com>
2023-10-22 14:53:49 +0300

    Ghange DebitPage

--- src/test/java/ru/netology/web/page/DebitPage.java
+++ src/test/java/ru/netology/web/page/DebitPage.java
@@ -2,6 +2,9 @@ package ru.netology.web.page;
 
 import com.codeborne.selenide.SelenideElement;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -14,6 +17,18 @@ public class DebitPage {
     private final SelenideElement nameInput = $(byText("Владелец"));
     private final SelenideElement cvcInput = $(byText("CVC/CVV"));
     private final SelenideElement continueButton = $(byText("Продолжить"));
+    private final SelenideElement successMassage = $(".notification_status_ok");
+    private final SelenideElement errorMassageForNumberOfCard = $(".input_invalid .input__sub");
+    private final SelenideElement errorMassageForMonth = $(".input-group__input-case_invalid .input__sub");
+    private final SelenideElement errorMassageForYear = $(". ");
+    private final SelenideElement errorMassageForCvc = $(". ");
 
 
+    public DebitPage() {
+        debitHead.shouldBe(visible);
+    }
+
+    public void verifySuccessMassage() {
+        successMassage.shouldBe(visible, Duration.ofSeconds(15));
+    }
 }
--- src/test/java/ru/netology/web/page/PaymentPage.java
+++ src/test/java/ru/netology/web/page/PaymentPage.java
@@ -1,6 +1,7 @@
 package ru.netology.web.page;
 
 import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -10,5 +11,13 @@ public class PaymentPage {
     private final SelenideElement debitButton = $("button");
     private final SelenideElement creditButton = $(byText("Купить в кредит"));
 
+    public DebitPage debitPayment(DataHelper.CardInfo info) {
+        debitButton.click();
+        return new DebitPage();
+    }
 
+    public DebitPage creditPayment(DataHelper.CardInfo info) {
+        creditButton.click();
+        return new DebitPage();
+    }
 }
