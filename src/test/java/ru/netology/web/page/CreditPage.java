0241e4bc998e265ba1537b37dc519db53e5aa497
Neto test <aryamorozkova555@gmail.com>
2023-10-22 22:21:34 +0300

    Change CreditPage

--- src/test/java/ru/netology/web/page/CreditPage.java
+++ src/test/java/ru/netology/web/page/CreditPage.java
@@ -1,6 +1,36 @@
 package ru.netology.web.page;
 
+import com.codeborne.selenide.SelenideElement;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
 public class CreditPage {
 
+    private final SelenideElement creditHead = $(byText("Кредит по данным карты"));
+    private final SelenideElement cardsNumberInput = $(byText("Номер карты"));
+    private final SelenideElement monthInput = $(byText("Месяц"));
+    private final SelenideElement yearInput = $(byText("Год"));
+    private final SelenideElement nameInput = $(byText("Владелец"));
+    private final SelenideElement cvcInput = $(byText("CVC/CVV"));
+    private final SelenideElement continueButton = $(byText("Продолжить"));
+    private final SelenideElement successMassage = $(".notification_status_ok");
+    private final SelenideElement errorMassageForInvalidNumberOfCard = $(".input_invalid .input__sub");
+    private final SelenideElement errorMassageForInvalidMonth = $(".input-group__input-case_invalid .input__sub");
+    private final SelenideElement errorMassageForInvalidYear = $(byText("Истёк срок действия карты"));
+    private final SelenideElement errorMassageForInvalidCvc = $(byText("Неверный формат"));
+    private final SelenideElement errorMassageForNullName = $(byText("Поле обязательно для заполнения"));
+    private final SelenideElement errorMassageForNullYear = $(byText("Неверный формат"));
 
+
+    public CreditPage() {
+        creditHead.shouldBe(visible);
+    }
+
+    public void verifySuccessMassage() {
+        successMassage.shouldBe(visible, Duration.ofSeconds(15));
+    }
 }
--- src/test/java/ru/netology/web/page/DebitPage.java
+++ src/test/java/ru/netology/web/page/DebitPage.java
@@ -18,10 +18,12 @@ public class DebitPage {
     private final SelenideElement cvcInput = $(byText("CVC/CVV"));
     private final SelenideElement continueButton = $(byText("Продолжить"));
     private final SelenideElement successMassage = $(".notification_status_ok");
-    private final SelenideElement errorMassageForNumberOfCard = $(".input_invalid .input__sub");
-    private final SelenideElement errorMassageForMonth = $(".input-group__input-case_invalid .input__sub");
-    private final SelenideElement errorMassageForYear = $(". ");
-    private final SelenideElement errorMassageForCvc = $(". ");
+    private final SelenideElement errorMassageForInvalidNumberOfCard = $(".input_invalid .input__sub");
+    private final SelenideElement errorMassageForInvalidMonth = $(".input-group__input-case_invalid .input__sub");
+    private final SelenideElement errorMassageForInvalidYear = $(byText("Истёк срок действия карты"));
+    private final SelenideElement errorMassageForInvalidCvc = $(byText("Неверный формат"));
+    private final SelenideElement errorMassageForNullName = $(byText("Поле обязательно для заполнения"));
+    private final SelenideElement errorMassageForNullYear = $(byText("Неверный формат"));
 
 
     public DebitPage() {
