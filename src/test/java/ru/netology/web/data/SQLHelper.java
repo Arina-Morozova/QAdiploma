421b18b3518ef311f2b4591a27629b75b56ec1cd
Neto test <aryamorozkova555@gmail.com>
2023-11-03 00:42:20 +0300

    Add Report.md

--- src/test/java/ru/netology/web/data/SQLHelper.java
+++ src/test/java/ru/netology/web/data/SQLHelper.java
@@ -2,6 +2,7 @@ package ru.netology.web.data;
 
 import lombok.SneakyThrows;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.Connection;
@@ -12,16 +13,22 @@ public class SQLHelper {
 
     private static final QueryRunner runner = new QueryRunner();
 
-    private static Connection getConn() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass");
+    public SQLHelper() {
     }
 
     @SneakyThrows
-    public String getDebitStatus() {
-        var debitStatusSQL = "SELECT status FROM payment_entity ORDER BY created DESC LIMIT 1";
-        var conn = getConn();
-        var debitStatus = runner.query(conn, debitStatusSQL, new ScalarHandler<String>());
-        return debitStatus;
+    public static Connection getConn() {
+        String url = System.getProperty("url");
+        String user = System.getProperty("user");
+        String password = System.getProperty("password");
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    @SneakyThrows
+    public static String getDebitStatus() {
+        String statusSQL = "SELECT status FROM payment_entity ORDER BY created DESC LIMIT 1";
+        Connection conn = getConn();
+        return runner.query(conn, statusSQL, new ScalarHandler<String>());
     }
 
     @SneakyThrows
--- src/test/java/ru/netology/web/test/DebitTest.java
+++ src/test/java/ru/netology/web/test/DebitTest.java
@@ -1,5 +1,6 @@
 package ru.netology.web.test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
